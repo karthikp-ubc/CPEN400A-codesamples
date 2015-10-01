a398b4b2c051c8e1853396143ed68840390e2489
karthikp-ubc <karthikp@ece.ubc.ca>
2015-10-01 11:39:07 -0700

    Simplified this based on what we covered in class

--- lecture3/shapes3.js
+++ lecture3/shapes3.js
@@ -6,15 +6,15 @@ function Point(x, y) {
 	this.dist = function(point) {
 		return Math.sqrt( (this.x - point.x) * (this.x - point.x)
 				+ (this.y - point.y) * (this.y - point.y) );
-	}	
+	};
+	return 0;	
 };
 
 Point.prototype.toString = function() {
 	return "(" + this.x + "," + this.y + ")";
-}
+};
 
 var add = function(p1, p2) {
-	console.log(this);
 	return new Point(p1.x + p2.x, p1.y + p2.y);
 };
 
@@ -25,24 +25,11 @@ var add2 = function(p1, p2) {
 	return res;
 };
 
-var addAll = function() {
-	var p = Object.create(this);
-	p.x = 0; p.y = 0;
-	for (var i = 0; i < arguments.length; i++) {
-		var point = arguments[i];
-		if (Object.getPrototypeOf(point)!=this) {
-			throw { name: TypeError, message: "Object " + point + " is not of type point" };
-		}
-		p.x += point.x;
-		p.y += point.y;
-	}
-	return p
-};
-
 var p1 = new Point(2, 3);
 var p2 = new Point(5, 7);
 console.log(Object.getPrototypeOf(p1) == Object.getPrototypeOf(p2));
-// console.log(Object.getPrototypeOf(p1).constructor);
+console.log(Object.getPrototypeOf(p1).constructor);
+
 document.writeln( "Distance between p1 and p2 = " + p1.dist(p2) );
 var res1 = add(p1, p2);
 console.log(res1);
@@ -57,12 +44,5 @@ var res3 = add2.call( Object.getPrototypeOf(p1), p1, p2 );
 console.log(res3); 
 document.writeln("Add2 of p1 and p2 = res3 = " + res3);
 
-var res4 = addAll.call( Object.getPrototypeOf(p1), p1, p2, res1, res2, res3);
-console.log(res4); 
-document.writeln("AddAll of p1,p2, res1, res2 and res3 = = " + res4);
 
-try {
-	var res5 = addAll.call( Object.getPrototypeOf(p1), p1, p2, "notapoint", res2, res3);
-} catch (e) {
-	document.writeln(e.name + " : " + e.message);
-};
+
