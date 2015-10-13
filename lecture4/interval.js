003b4c78d041bb9b1c8bde7dc89f165c4063ff4f
karthikp-ubc <karthikp@ece.ubc.ca>
2015-10-13 09:15:18 -0700

    Solution to the class activity using setInterval
    Alternate solution

---
+++ lecture4/interval.js
@@ -0,0 +1,20 @@
+// Solution to the class activity using setItnerval
+
+var invokeTimes = function(func, noTimes, time) {
+	console.log("Setting up interval " + noTimes + " " + time);
+	var count = 0;
+	var interval;
+	var intervalHandler = function() {
+		console.log( "invocation " + count);
+		func(count);
+		count = count + 1;
+		if (count == noTimes) {
+			clearInterval(interval);
+		}
+	};
+	interval = setInterval(intervalHandler, time);
+};
+
+var setup = function() {
+	invokeTimes( function(i) { alert("hello " + i); }, 10, 1000 );
+}
