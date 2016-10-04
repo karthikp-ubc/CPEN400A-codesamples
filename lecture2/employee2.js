889ac81be9989e5e72686e5f09fb10c431c00603
karthikp-ubc <karthikp@ece.ubc.ca>
2016-10-04 11:57:07 -0700

    Illustrated other syntax

--- lecture2/employee2.js
+++ lecture2/employee2.js
@@ -1,26 +1,33 @@
 // Object creation using Object.create
 
 var Person = {
-	firstName: "John",
-	lastName: "Smith",
-	gender: "Male",
 	print: function() {
 		console.log("Person " + this.firstName + " " + this.lastName + " " + this.gender );
 	}
 };
 
 var p = Object.create(Person);
+p.firstName = "Linda";
+p.lastName = "James";
+p.gender = "female";
+
 p.print();
 console.log(p);
 console.log( Object.getPrototypeOf(p) );
 console.log( p.prototype );	
 
-var e = Object.create(p);
+var e = Object.create(Person, {
+		firstName: {value: "John"},
+		lastName: {value: "Smith"},
+		gender: {value: "Male"}
+	}
+);
+e.title = "manager";
 e.print = function() {
 		console.log("Employee " + this.title);
 		Person.print(); 
 };
-e.title = "manager";
+
 e.print();
 console.log(e);
 console.log( Object.getPrototypeOf(e) );	
