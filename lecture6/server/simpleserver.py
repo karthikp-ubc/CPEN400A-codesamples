9d43f98cd966798752ea7e36ccf072626f5a9169
karthikp-ubc <karthikp@ece.ubc.ca>
2016-11-01 14:16:37 -0700

    Minor

--- lecture6/server/simpleserver.py
+++ lecture6/server/simpleserver.py
@@ -12,11 +12,11 @@ PORT = 8080	# Port to connect to
 
 # Begin configuration - change these parameters for the server to have interesting behaviors
 
-delay = True	# Should there be a delay ?
-minDelay = 0 	# if so, the minimum delay in seconds
-maxDelay = 3 	# if so, the maximum delay in seconds
-introduceError = True # Should we have the server returning an error
-errorProb = 0.50	# With what probability should we introduce errors
+delay = True		# Should there be a delay ?
+minDelay = 0 		# if so, the minimum delay in seconds
+maxDelay = 5 		# if so, the maximum delay in seconds
+introduceError = True 	# Should we have the server returning an error
+errorProb = 0.50	# If so, with what probability should we introduce errors
 
 # End of configuration - change the code below only if you know what you're doing
 
