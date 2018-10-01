2c844eef02b63977b8e429b1bd40036bff8ffe10
karthikp-ubc <karthikp@ece.ubc.ca>
2018-09-30 21:02:06 -0700

    Got rid of unnecessary cruft

--- lecture3/higher-order.js
+++ lecture3/higher-order.js
@@ -4,9 +4,7 @@ var map = function( array, fn ) {
 	// Applies fn to each element of list, returns a new list
 	var result = [];
 	for (var i = 0; i < array.length; i++) {
-		var element = array[i];
-		var args = [ element ];
-		result.push( fn.apply(null, args) );
+		result.push( fn(array[i]) );
 	}
 	return result;
 };
@@ -22,22 +20,4 @@ var add = function(a, b) {
 var add10 = add.bind(null, 10); 
 document.writeln( map(l, add10) )
 
-// Solution to the class activity for the filter function is below;
-var filter = function( array, fn ) {
-        var result = [];
-        for (var i = 0; i < array.length; i++) {
-                var element = array[i];
-                var args = [ element ];
-                if (fn.apply(null, args) ) result.push(element); 
-        }
-        return result;
-};
-
-var lesserThan = function(a, b) { return (a < b) ? true:false; } 
-var greaterThan5 = lesserThan.bind(null, 5);
-
-var a = [ 1, 3, 10, 8, 2, 7, 6 ];
-document.writeln(a);
-var c = filter( a, greaterThan5);
-document.writeln(c);
 	
