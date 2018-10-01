6f12a984b01da21606ffd8dd4cbb1477855e5dd8
karthikp-ubc <karthikp@ece.ubc.ca>
2018-09-30 21:08:39 -0700

    Added solution to class activity for filter

---
+++ lecture3/higher-order2.js
@@ -0,0 +1,22 @@
+	
+// Solution to the class activity for the filter function is below;
+var filter = function( array, fn ) {
+        var result = [];
+        for (var i = 0; i < array.length; i++) {
+            if (fn(array[i])) result.push(element); 
+        }
+        return result;
+};
+
+var lesserThan = function(a, b) { 
+		return (a < b) ? true:false; 
+	}
+
+var greaterThan5 = lesserThan.bind(null, 5);
+
+var a = [ 1, 3, 10, 8, 2, 7, 6 ];
+document.writeln(a);
+
+var c = filter( a, greaterThan5);
+document.writeln(c);
+	
