161b2be9d1dfe81df9c18c4a3c83695f7bfebd78
karthikp-ubc <karthikp@ece.ubc.ca>
2018-10-07 15:39:58 -0700

    Made the example more complex

--- lecture4/prop.js
+++ lecture4/prop.js
@@ -20,17 +20,26 @@ window.onload = function() {
 	var div2 = document.getElementById("two");
 	var div3 = document.getElementById("three");
 	var div4 = document.getElementById("four");
+	var div5 = document.getElementById("five");
+	var div6 = document.getElementById("six");
+	var div7 = document.getElementById("seven");
 	var btn = document.getElementById("btn");
 
 	div1.addEventListener("click", bubbleHandler("div1"), false);
 	div2.addEventListener("click", bubbleHandler("div2"), false);
 	div3.addEventListener("click", bubbleHandler("div3"), false);
 	div4.addEventListener("click", bubbleHandler("div4"), false);
+	div5.addEventListener("click", bubbleHandler("div5"), false);
+	div6.addEventListener("click", bubbleHandler("div6"), false);
+	div7.addEventListener("click", bubbleHandler("div7"), false);
 	btn.addEventListener("click", bubbleHandler("btn"), false);
 
 	div1.addEventListener("click", captureHandler("div1"), true);
 	div2.addEventListener("click", captureHandler("div2"), true);
 	div3.addEventListener("click", captureHandler("div3"), true);
 	div4.addEventListener("click", captureHandler("div4"), true);
+	div5.addEventListener("click", bubbleHandler("div5"), true);
+	div6.addEventListener("click", bubbleHandler("div6"), true);
+	div7.addEventListener("click", bubbleHandler("div7"), true);
 	btn.addEventListener("click", captureHandler("btn"), true);
 };
