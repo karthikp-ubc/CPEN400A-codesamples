97cbf37e20656121fea26567110192e3f07b6807
karthikp-ubc <karthikp@ece.ubc.ca>
2018-10-28 18:20:21 -0700

    Fixed bug with throw

--- lecture8/wordCount-noStream.js
+++ lecture8/wordCount-noStream.js
@@ -2,9 +2,12 @@ var fs = require("fs");
 var length = 0;
 var fileName = "sample.txt";
 fs.readFile(fileName, function(err, buf) {
-		if (err) throw err;	
-		length = buf.length;;
-		console.log("Number of characters read = " + length);
+		if (err) {
+			console.log("Error in reading file " + err);
+		} else {	
+			length = buf.length;;
+			console.log("Number of characters read = " + length);
+		}
 	}
 );
 
