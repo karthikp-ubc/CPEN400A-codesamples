f72a23505d19f1f7a10336f3b2ff4d9e88266341
karthikp-ubc <karthikp@ece.ubc.ca>
2019-10-15 11:37:55 -0700

    Minor

--- lecture5/divextension.js
+++ lecture5/divextension.js
@@ -3,9 +3,9 @@
 var wrapGetElementById  = function() {
 	var origGebid = document.getElementById;
 	document.getElementById = function(id) {
-		// console.log("Calling wrapper function with " + id);
+		console.log("Calling wrapper function with " + id);
 		var node = origGebid.call(document, id);
-		if (node && node["count"]!=undefined) {
+		if (node && node["count"]!="undefined") {
 			node.count = node.count + 1;
 		}
 		return node;
@@ -14,8 +14,7 @@ var wrapGetElementById  = function() {
 
 var initDivElements = function() {
 	var divs = document.getElementsByTagName("div");
-	for (var i = 0; i < divs.length; i++) {				
-		var div = divs[i];
+	for (var i = 0; i < divs.length; i++) {					   var div = divs[i];
 		div.count = 0;
 		console.log(div.count);
 	}
