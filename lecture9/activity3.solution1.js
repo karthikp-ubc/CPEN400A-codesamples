40741dbc915cb7bc35f1e57732e3a1148c11f2d7
karthikp-ubc <karthikp@ece.ubc.ca>
2019-11-06 23:02:55 -0800

    Used arrow functions

--- lecture9/activity3.solution1.js
+++ lecture9/activity3.solution1.js
@@ -4,16 +4,12 @@ if (! fs) process.exit(1);
 
 function readFile(fileName) {
 	// Return a promise which reads the file and is resolved (or is rejected)
-	return new Promise(function(resolve, reject) {
-		console.log("Creating a new promise");
-		fs.readFile(fileName, function(err, buf) {
-			if (err) {
-				console.log("Rejecting the promise");
+	return new Promise( (resolve, reject) => {
+		fs.readFile(fileName, (err, buf) => {
+			if (err) 
 				reject(err);
-			} else {
-				console.log("Resolving the promise");
+			else 
 				resolve(buf);
-			}	
 		}); // End of readFile 
 	}); // End of promise
 };
@@ -22,31 +18,16 @@ var outputName = "sample-out.txt"
 var p1 = readFile("sample.txt");
 var p2 = readFile("sample.txt");
 
-function doneReading(buf) {
-	console.log("Read " + buf.length + " characters");
-	return buf;
-};
-
-function errorReading(err) {
-	console.log("Error reading file " + err);
-	return null;
-};
-
-p1.then(doneReading).catch(errorReading);
-p2.then(doneReading).catch(errorReading);
-
 var p3 = Promise.all([p1, p2]);
 p3.then( function(bufs) {
-		console.log("Both files read");
 		var concatenated = "", total = 0;
 		for (var i=0; i<bufs.length; i++) {
 			concatenated = concatenated + bufs[i];
 			total += bufs[i].length;
 		}
-		fs.writeFile(outputName, concatenated, function(err) {
+		fs.writeFile(outputName, concatenated, (err) => {
 			if (err) throw Error("Error writing file: " + err);			
 		});
-		console.log("Wrote " + total + " bytes");
 	}
 ).catch( function(error) {
 	console.log("Encountered error " + error.message);
