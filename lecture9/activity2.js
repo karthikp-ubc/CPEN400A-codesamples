13ccd09769999a3ea4a351aa5ddf0e3007f5f529
karthikp-ubc <karthikp@ece.ubc.ca>
2019-11-06 22:35:40 -0800

    Cleaned up the code

--- lecture9/activity2.js
+++ lecture9/activity2.js
@@ -3,12 +3,6 @@ var fs = require("fs");
 // you can use fs.readFile
 function readFile (filepath){
     // to implement
-    return new Promise( (resolve, reject) => {
-        fs.readFile(filepath, 
-		(err, data) => { if (err) reject(err) 
-				else resolve(data); }
-	);
-    });
 }
 
 // testing the functionality
