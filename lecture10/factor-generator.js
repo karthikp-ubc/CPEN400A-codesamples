0e92d8fd0b5dfff02a3e179513c49902010bc036
Karthik <karthikp@ece.ubc.ca>
2020-10-25 14:24:30 -0700

    Solution to class activity

---
+++ lecture10/factor-generator.js
@@ -0,0 +1,19 @@
+// Generate all the factors of a given number in ascending order until that number
+
+function *factorGen(num) {
+    if (num<=0) throw("number must be > 0");
+    // It's sufficient to go until cieling(n/2)
+    for (var i=1; i<(num/2 + 1); ++i) {
+        if (num % i == 0) yield i;
+    } 
+    if (num>1) yield num;
+}
+
+var fg = factorGen(1000);
+try {
+    for (x of fg) {
+        console.log("Factor: " + x);
+    }
+} catch(e) {
+    console.log("Caught exception : " + e);
+}
