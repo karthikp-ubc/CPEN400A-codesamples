388cc0e707dd9eeaa2d3887ea7491f4f69ce72d1
Karthik <karthikp@ece.ubc.ca>
2022-11-02 20:37:44 -0700

    Minor

--- lecture9/activity3-test.js
+++
@@ -1,9 +0,0 @@
-var myLib = require('./activity3.js');
-
-myLib.trainModel("ABC")
- .then((result)=> {
-    console.log("The result of trainModel is:");
-    console.log(result);
-  })
- .catch(console.log)
- .finally(()=> console.log("trainModel finished!"));
